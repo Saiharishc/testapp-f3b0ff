dcbcc63a895eab76818472a08f8d1940011cce06
Saiharishc <51015572+Saiharishc@users.noreply.github.com>
2026-07-06 08:29:14 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def read_root():
+    return {"message": "Hello World"}
+
+@app.get('/items')
+async def list_items():
+    return []
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {"item_id": item_id}
